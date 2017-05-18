4ba3382cd7c6245d1fffe381860ed62171c0c9d7
shravancr95 <shravan.rad@gmail.com>
2017-05-18 16:39:40 -0700

    Initial Commit

---
+++ PokedexGo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PokedexGo
+//
+//  Created by Radhakrishna Canchi on 5/18/17.
+//  Copyright © 2017 Shravan Canchi Radhakrishna. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
